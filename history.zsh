5dad69178aac29bbd5c1e88c8a59a2ddf91280df
Julien Roche <julien.roche14@gmail.com>
2016-02-13 19:07:22 +0100

    (master) history

--- history.zsh
+++ history.zsh
@@ -5,8 +5,8 @@ HISTFILE=~/.zsh_history
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
 
-# autoload up-line-or-beginning-search
-# autoload down-line-or-beginning-search
+autoload up-line-or-beginning-search
+autoload down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search
